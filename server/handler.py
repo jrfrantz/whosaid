01e9c30a7e9e31a60506dc40dbfb2aa073ac1de4
Bob sami <robertsami@robertsami-macbookpro.roam.corp.google.com>
2014-10-05 13:36:31 -0700

    get the handler to serve static content

--- server/handler.py
+++ server/handler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from os import curdir, sep
 
 PORT_NUMBER = 8080
 
@@ -9,12 +10,46 @@ class myHandler(BaseHTTPRequestHandler):
 	
 	#Handler for the GET requests
 	def do_GET(self):
-		self.send_response(200)
-		self.send_header('Content-type','text/html')
-		self.end_headers()
-		# Send the html message
-		self.wfile.write("Hello World !")
-		return
+		if self.path=="/":
+			self.path="/index_example2.html"
+
+		try:
+			#Check the file extension required and
+			#set the right mime type
+
+			sendReply = False
+			if self.path.endswith(".html"):
+				mimetype='text/html'
+				sendReply = True
+			if self.path.endswith(".jpg"):
+				mimetype='image/jpg'
+				sendReply = True
+			if self.path.endswith(".gif"):
+				mimetype='image/gif'
+				sendReply = True
+			if self.path.endswith(".js"):
+				mimetype='application/javascript'
+				sendReply = True
+			if self.path.endswith(".css"):
+				mimetype='text/css'
+				sendReply = True
+			if self.path.endswith(".mp3"):
+				mimetype='audio/mpeg'
+				sendReply = True
+
+			if sendReply == True:
+				#Open the static file requested and send it
+				f = open(curdir + sep + self.path) 
+				self.send_response(200)
+				self.send_header('Content-type',mimetype)
+				self.end_headers()
+				self.wfile.write(f.read())
+				f.close()
+			return
+
+
+		except IOError:
+			self.send_error(404,'File Not Found: %s' % self.path)
 
 try:
 	#Create a web server and define the handler to manage the
