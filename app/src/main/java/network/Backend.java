84516cd8072cc8b399eecaa2e5be71bf53fd50c7
Bob sami <robertsami@robertsami-macbookpro.roam.corp.google.com>
2014-10-05 18:42:39 -0700

    almost finished backend

--- app/src/main/java/network/Backend.java
+++ app/src/main/java/network/Backend.java
@@ -1,10 +1,16 @@
 package network;
 import com.parse.*;
 
-//import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jacob on 10/5/14.
@@ -14,9 +20,8 @@ public class Backend {
 
     private static final String info_map = "USER_TO_FILE_MAP";
 
-
     public Backend() {
-        // read
+        // TODO: a lot of this
         ParseQuery<ParseObject> query = ParseQuery.getQuery(info_map);
         query.getInBackground("xWMyZ4YEGZ", new GetCallback<ParseObject>() {
             public void done(ParseObject object, ParseException e) {
@@ -29,42 +34,65 @@ public class Backend {
         });
     }
 
-    // TODO(rob): this
     public void deleteFiles(User user) {
-
+        if (!fileMap.containsKey(user)) {
+            return;
+        }
+        UserInfo i = fileMap.get(user);
+        i.deleteInBackground();
+        fileMap.remove(user);
     }
 
-    // TODO(rob): this
+    // TODO: look for collisions
     private String generateRandomFileName() {
-        return null;
+        Random r = new Random();
+        // TODO: format this string to fix the width of integers
+        String s = "guid_" + r.nextInt();
+        return s;
     }
 
     public void saveFiles(User user, File picture, File voice) {
-        if (fileMap.containsKey(user)) {
-            deleteFiles(user);
-        }
+        UserInfo info = fileMap.containsKey(user) ? fileMap.get(user) : new UserInfo();
 
         String pictureFileName = generateRandomFileName();
         String voiceFileName   = generateRandomFileName();
-        UserInfo info = new UserInfo();
         info.setPictureFileName(pictureFileName);
         info.setPictureFileName(voiceFileName);
 
-        // TODO: import commons io and use byte function
         byte[] pictureFileBytes = null;
+        try {
+            pictureFileBytes = FileUtils.readFileToByteArray(picture);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
         byte[] voiceFileBytes = null;
+        try {
+            voiceFileBytes = FileUtils.readFileToByteArray(voice);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
 
         ParseFile pictureFile = new ParseFile(pictureFileName, pictureFileBytes);
         ParseFile voiceFile   = new ParseFile(voiceFileName, voiceFileBytes);
 
         pictureFile.saveInBackground();
         voiceFile.saveInBackground();
-
-
     }
 
     private void writeBytesToFile(File f, byte[] data) {
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
+        try {
+            fos.write(data);
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private File getPicture(User user) {
@@ -126,8 +154,19 @@ public class Backend {
         return pair;
     }
 
-    public User[] getRandomUsers(int numUsers) {
-        return null;
+    // TODO: make this more efficient, i.e. don't use contains
+    public List<User> getRandomUsers(int numUsers) {
+        List<User> keysAsArray = new ArrayList<User>(fileMap.keySet());
+        Random r = new Random();
+
+        List<User> newUsers = new ArrayList<User>();
+        while (newUsers.size() < numUsers) {
+            User u = keysAsArray.get(r.nextInt(keysAsArray.size()));
+            if (!keysAsArray.contains(u)) {
+                newUsers.add(u);
+            }
+        }
+        return newUsers;
     }
 }
 
