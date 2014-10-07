7f4219860403373f938d414231a44b60aa90a782
Bob sami <robertsami@robertsami-macbookpro.roam.corp.google.com>
2014-10-06 23:17:42 -0700

    updating data import

--- data_import/send_files.py
+++ data_import/send_files.py
@@ -2,6 +2,7 @@ import json,httplib
 
 who_said_app_id  = "8Oj7j7pYRXClaPfwr93VEcx4izkSDTk5dSgLzXBA"
 who_said_api_key = "qUMfW8e1A0idm5zVGwpplNAD1ML9jWhr9lZDVMlW"
+user_map_obj_id  = "DGBUeNQwMD" #TODO(rob) read last one in user_map.txt
 
 # expects picture.jpg and sound.mp3 in folder
 def send_pair(local_path_to_folder):
@@ -9,17 +10,19 @@ def send_pair(local_path_to_folder):
   print res1
   picture_name = res1["name"]
   res2 = send_sound_byte(local_path_to_folder + "/sound.mp3")
-  picture_name = res2["name"]
+  voice_name = res2["name"]
   print res2
 
   #get object name from end of path
   obj_name = local_path_to_folder.split("/")[-1]
   print obj_name
+  res3 = associate_with_user_info(picture_file_name = picture_name, voice_file_name = voice_name)
+  print res3
 
 def send_picture(local_path):
   connection = httplib.HTTPSConnection('api.parse.com', 443)
   connection.connect()
-  connection.request('POST', '/1/files/pic.jpg', open(local_path, 'rb').read(), {
+  connection.request('POST', '/1/files/picture.jpg', open(local_path, 'rb').read(), {
          "X-Parse-Application-Id": who_said_app_id,
          "X-Parse-REST-API-Key": who_said_api_key,
          "Content-Type": "image/jpeg"
@@ -31,7 +34,7 @@ def send_picture(local_path):
 def send_sound_byte(local_path):
   connection = httplib.HTTPSConnection('api.parse.com', 443)
   connection.connect()
-  connection.request('POST', '/1/files/pic.jpg', open(local_path, 'rb').read(), {
+  connection.request('POST', '/1/files/sound.jpg', open(local_path, 'rb').read(), {
          "X-Parse-Application-Id": who_said_app_id,
          "X-Parse-REST-API-Key": who_said_api_key,
          "Content-Type": "audio/mpeg"
@@ -39,13 +42,16 @@ def send_sound_byte(local_path):
   result = json.loads(connection.getresponse().read())
   return result
 
-def associate_with_parse_obj():
+def associate_with_user_info(picture_file_name, voice_file_name):
   connection = httplib.HTTPSConnection('api.parse.com', 443)
   connection.connect()
-  connection.request('POST', '/1/classes/PlayerProfile', json.dumps({
-         "name": "Andrew",
-         "picture": {
-           "name": "...profile.png",
+  connection.request('POST', '/1/classes/UserInfo', json.dumps({
+         "voiceFile": {
+           "name": voice_file_name,
+           "__type": "File"
+         },
+         "pictureFile": {
+           "name": picture_file_name,
            "__type": "File"
          }
        }), {
@@ -55,5 +61,32 @@ def associate_with_parse_obj():
        })
   result = json.loads(connection.getresponse().read())
   return result
+
+# def associate_with_user_map(user_info_obj_id, user_name, user_info_obj_id):
+#   connection = httplib.HTTPSConnection('api.parse.com', 443)
+#   connection.connect()
+#   connection.request('PUT', '/1/classes/GameScore/' + user_map_obj_id, json.dumps({
+#          user_name: user_info_obj_id
+#        }), {
+#          "X-Parse-Application-Id": who_said_app_id,
+#          "X-Parse-REST-API-Key": who_said_api_key,
+#          "Content-Type": "application/json"
+#        })
+#   result = json.loads(connection.getresponse().read())
+#   print result
+
+# DONT CALL THIS SHIT OK?
+def create_user_map():
+  connection = httplib.HTTPSConnection('api.parse.com', 443)
+  connection.connect()
+  connection.request('POST', '/1/classes/UserMapObject', json.dumps({
+       }), {
+         "X-Parse-Application-Id": who_said_app_id,
+         "X-Parse-REST-API-Key": who_said_api_key,
+         "Content-Type": "application/json"
+       })
+  result = json.loads(connection.getresponse().read())
+  # TODO(rob): make this append to user_map.txt
+  print result
 
 send_pair("/Users/robertsami/Documents/dev/whosaid/server/res/troll")
